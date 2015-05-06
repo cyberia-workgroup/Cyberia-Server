18baa1e906a4a4b8274c091eb26a46b328c2fc99
BDHesse <BDHesse@gmail.com>
2015-05-05 21:45:39 -0400

    Added user testData

---
+++ tests/testData/user.js
@@ -0,0 +1,16 @@
+var testData = testData || {};
+
+(function (testData, undefined) {
+    'use strict';
+    testData.createCharacterProfile = {
+        requestMethod: 'POST',
+        requestUrl:'/me/profiles',
+        request: {
+
+        },
+        response: {}
+    }
+
+})(testData);
+
+module.exports = testData;
