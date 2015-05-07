4d38cd0c1f2b717a90f59c2e63c80f572b843e2b
Robert Gladson <rkgladson@gmail.com>
2015-05-07 01:29:17 -0400

    - Adding description for user routes.

---
+++ tests/testData/_headers.js
@@ -0,0 +1,7 @@
+var header = {}, cookie;
+
+header.cookie = cookie = {};
+
+cookie.auth = '<SHA-1>';
+
+testData.header  = header;
--- tests/testData/user.js
+++ tests/testData/user.js
@@ -1,4 +1,63 @@
-var profile = {};
+var profile = {}, user = {};
+
+user.create = {
+    method:'POST',
+    requestUrl: '/register',
+    statusCode: 200, //OK
+    request: {
+        // FIGURE OUT WHAT GOES HERE!
+        name: "Jane Doe",
+        email: "example@example.com"
+    },
+    response: {
+        name: "Jane Doe",
+        email: "example@example.com",
+        accountType: '<oauth type>'
+    }
+};
+
+user.read = {
+    method: 'GET',
+    requestUrl: "/user/<userId>",
+    statusCode: 200, // OK
+    request: null,
+    response: {
+        userId: '<userId>',
+        name: "Jane Doe",
+        profiles: [
+            {
+                profileId: "<profileId>",
+                name: 'characterName',
+                gender: 'not specified'
+            }
+        ]
+    }
+};
+
+user.update = {
+    method: 'PUT',
+    requestUrl: '/me',
+    statusCode: 200, // OK
+    cookies: {auth: cookie.auth},
+    request: {
+        email: 'example@example.com'
+    },
+    response : {
+        email: 'example@example.com',
+    }
+};
+
+user.delete = {
+    method: 'DELETE',
+    requestUrl:'/me/',
+    cookies: {auth: cookie.auth},
+    request: null,
+    response: null,
+    statusCode: 204 // No Content
+}
+
+
+
 profile.create = {
     method: 'POST',
     requestUrl:'/me/profiles',
@@ -33,4 +92,6 @@ profile.delete = {
     response: null,
     statusCode: 204 // No Content
 };
+
 testData.profile = profile;
+testData.user = user;
